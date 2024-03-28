4ac6da15f56c063a823deb1b8f252d714bd1c0ee
user <lekim.pb@gmail.com>
2024-03-28 20:05:49 +0100

    Finish Circle Tracking

--- src/TP5/scripts/track_n_move_partial.py
+++ src/TP5/scripts/track_n_move_partial.py
@@ -31,7 +31,6 @@ class Node:
         # Topic subscription
         rgb_sub = message_filters.Subscriber('/camera/rgb/image_raw', Image)
         depth_sub = message_filters.Subscriber('/camera/depth/image_raw', Image)
-
         # Synchronization of received messages
         ats = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], queue_size=5, slop=0.1)
         ats.registerCallback(self.callback)
@@ -157,42 +156,55 @@ class Node:
         :param img: opencv image
         :return img: processed opencv image
         """
-	while not rospy.is_shutdown():
-	    # Circle detection
+	#while not rospy.is_shutdown():
+		# Circle detection
 	    # Your code starts here
-	    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 	    # define range of blue color in HSV
-	    lower_red = np.array([0,100,100])
-	    upper_red = np.array([10,255,255])
+	lower_red = np.array([0,100,100])
+	upper_red = np.array([10,255,255])
 	    # Threshold the HSV image to get only blue colors
-	    mask = cv2.inRange(hsv, lower_red, upper_red)
+	mask = cv2.inRange(hsv, lower_red, upper_red)
 	    # Bitwise-AND mask and original image
-	    filtered_img = cv2.bitwise_and(img,img, mask= mask)
-	    gray_img = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
-	    blurred_img = cv2.medianBlur(gray_img,5)
-	    circles = cv2.HoughCircles(blurred_img,cv2.HOUGH_GRADIENT,1,filtered_img.shape[0]*2, param1=300, param2=1)
-	    center = [0,0]
+	filtered_img = cv2.bitwise_and(img,img, mask= mask)
+	gray_img = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
+	blurred_img = cv2.medianBlur(gray_img,5)
+	circles = cv2.HoughCircles(blurred_img,cv2.HOUGH_GRADIENT,1,filtered_img.shape[0]*2, param1=300, param2=1)
 	    #print circles
-            # Draw detected circles on the original image
-            if circles is not None:
-		circles = np.uint16(np.around(circles))
-		for i in circles[0,:]:
+	    # Draw detected circles on the original image
+	if circles is not None:
+	    circles = np.uint16(np.around(circles))
+	    for i in circles[0,:]:
 		 # draw the outer circle
-		    cv2.circle(filtered_img,(i[0],i[1]),i[2],(0,255,0),2)
+	        cv2.circle(blurred_img,(i[0],i[1]),i[2],(0,255,0),2)
 		 # draw the center of the circle
-		    center[0] = i[0]
-		    center[1] = i[1]
-		    cv2.circle(filtered_img,(i[0],i[1]),2,(0,0,255),3)
-	    
-	    #if np.shape(center) == 2:
-	    print center[0], center[1]
-	    print depth[center[0]][center[1]]
-	    self.image_pub.publish(self.bridge.cv2_to_imgmsg(filtered_img, encoding = 'passthrough'))
-	    cv2.imshow("Processed Image", filtered_img)
-       	    cv2.waitKey(1)
+		cv2.circle(blurred_img,(i[0],i[1]),2,(0,0,255),3)
+	self.image_pub.publish(self.bridge.cv2_to_imgmsg(blurred_img, encoding = 'passthrough'))
+	cv2.imshow("Processed Image", blurred_img)
+	cv2.waitKey(1)
+	
 	# Publish necessary commands
         linear = 0.
         angular = 0.
+	if circles is None:
+	    angular = 0.5
+	else:
+	    max_radius = 0
+	    center = []
+	    # This code to ensure we take the circle with biggest radius
+	    for i in circles[0,:]:
+		if i[2] > max_radius: 
+		    max_radius = i[2]
+		    center = i
+	    if ((center[0] - img.shape[1]/2) > 50):
+		print center[0] - img.shape[1]/2
+		angular = -0.2
+	    elif ((img.shape[0]/2 - center[1]) > 50):
+		print center[0] - img.shape[1]/2
+		angular = 0.2
+	    else:
+	    	if (depth[center[0]][center[1]] > 0.4):
+		    linear = 0.2
         self.send_commands(linear, angular)        
         return img
 	
